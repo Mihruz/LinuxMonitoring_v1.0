10b11072fc3f80aa90ddd17d54acd40d1331ef1d
prunesga <mihruz@gmail.com>
2023-10-26 17:39:42 +0300

    10

--- src/04/input.sh
+++ src/04/input.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-if [ "$#" -ne 4 ]; then
-  echo "Please provide four numeric parameters (from 1 to 6) for background and text colors."
+if [ "$#" -ne 0 ]; then
+  echo "Please do not use any parameters. Parameters must be specified in the config file."
   exit 1
 fi
 
-for param in "$@"; do
-  if ! [[ "$param" =~ ^[1-6]$ ]]; then
-    echo "Invalid parameter $param. Please use a number from 1 to 6."
-    exit 1
-  fi
-done
+default_column1_background=6
+default_column1_font_color=1
+default_column2_background=2
+default_column2_font_color=4
 
-column1_background=$1
-column1_font_color=$2
-column2_background=$3
-column2_font_color=$4
+column1_background="${column1_background:-$default_column1_background}"
+column1_font_color="${column1_font_color:-$default_column1_font_color}"
+column2_background="${column2_background:-$default_column2_background}"
+column2_font_color="${column2_font_color:-$default_column2_font_color}"
 
 if [[ $column1_background == $column1_font_color || $column2_background == $column2_font_color ]]; then
-  echo "Error: font and background colors must not match."
+  echo "Error: font and background colors must not match. Using default colors."
+  column1_background=$default_column1_background
+  column1_font_color=$default_column1_font_color
+  column2_background=$default_column2_background
+  column2_font_color=$default_column2_font_color
   exit 1
 fi
--- src/04/main.sh
+++ src/04/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+source config.txt
 source input.sh
 source sys_info.sh
 source colors.sh
-
